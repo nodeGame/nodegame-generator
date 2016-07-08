f8b08d9b439e4ac580fbf15a129aebe0b77b08de
Stefano Balietti <futur.dorko@gmail.com>
2016-07-08 12:32:32 -0400

    mods

--- games/dictator/requirements/requirements.room.js
+++ games/dictator/requirements/requirements.room.js
@@ -1,9 +1,11 @@
 /**
- * # Standard Requirements room
+ * # Requirements Room
  * Copyright(c) {YEAR} {AUTHOR} <{AUTHOR_EMAIL}>
  * MIT Licensed
  *
- * http://www.nodegame.org
+ * Validates the requirements of incoming connections
+ *
+ * http://nodegame.org
  * ---
  */
 module.exports = function(settings, room, runtimeConf) {
@@ -14,9 +16,27 @@ module.exports = function(settings, room, runtimeConf) {
     // Creates a stager object to define the game stages.
     var stager = new node.Stager();
 
-    // Functions.
+    var nextRoom;
+    if ('undefined' === typeof settings.nextRoom) {
+        // This should be safe, because waitRoom is created before.
+        if (channel.waitingRoom) {
+            nextRoom = channel.waitingRoom.name;
+        }
+        else {
+            throw new Error('requirements.room: no nextRoom provided, and ' +
+                            'no waiting room found.');
+        }
 
-    settings.doChecking = true;
+    }
+    else if ('string' === typeof settings.nextRoom &&
+             settings.nextRoom !== '') {
+
+        nextRoom = settings.nextRoom;
+    }
+    else {
+        throw new TypeError('requirements.room: nextRoom must be undefined ' +
+                            'or non-empty string. Found: ' + settings.nextRoom);
+    }
 
     function connectingPlayer(player) {
         console.log('Player connected to Requirements room.', player.id);
@@ -39,7 +59,9 @@ module.exports = function(settings, room, runtimeConf) {
         }, 500);
     }
 
-    function init() {
+    // Define stager.
+
+    stager.setOnInit(function init() {
         var that = this;
 
         node.on.preconnect(function(player) {
@@ -64,43 +86,34 @@ module.exports = function(settings, room, runtimeConf) {
                 registry.updateClient(msg.from, {apt: true});
 
                  setTimeout(function() {
-                     channel.moveClient(msg.from, channel.waitingRoom.name);
+                     channel.moveClient(msg.from, nextRoom);
                  }, 1000);
             }
         });
 
-    }
-
-    stager.addStage({
-        id: 'requirements',
-        cb: function() {
-            console.log('Requirements room created: ' + channel.name + '.');
-        }
     });
 
-    // Define stager.
+    stager.setDefaultProperty('publishLevel', 0);
 
-    stager.setOnInit(init);
-
-    stager.next('requirements');
+    stager.next({
+        id: 'requirements',
+        cb: function() {
+            console.log('Requirements room created: ' + channel.name);
+        }
+    });
 
     // Return the game.
     game = {};
 
     game.metadata = {
         name: 'Requirements check room',
-        description: 'Validates players requirements.',
-        version: '0.1'
+        description: 'Validates clients requirements.',
+        version: '0.3.0'
     };
 
-    // Throws errors if true.
-    game.debug = true;
-
+    game.debug = settings.debug || false;
     game.plot = stager.getState();
-
     game.nodename = 'requirements';
-
-    // game.verbosity = 100;
-
+    game.verbosity = 0;
     return game;
 };
